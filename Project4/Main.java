43d0ff2b0e97a4bcaeeff8897ed2dd46cbd4ac7a
Ramirezzz1 <88006775+Ramirezzz1@users.noreply.github.com>
2025-01-13 19:48:17 -0500

    Add files via upload
    skeleton code for main function use to run complex.java

---
+++ Project4/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main{
+    public static void main(String[] args) {
+        Complex a = new Complex(1,2);
+        Complex b = new Complex(1,3);
+
+        int i = 5;
+
+        System.out.println()
+        System.out.println()
+        System.out.println()
+        System.out.println()
+
+        System.out.println()
+        System.out.println()
+        System.out.println()
+        System.out.println()
+
+
+
+    }
+}
