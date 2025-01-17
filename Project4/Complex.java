5b91188008e865f4d6463c13cc855fdc82194b78
Ramirezzz1 <88006775+Ramirezzz1@users.noreply.github.com>
2025-01-17 00:03:09 -0500

    Update Complex.java
    brought over main method

--- Project4/Complex.java
+++ Project4/Complex.java
@@ -86,4 +86,22 @@ public class Complex{
 
     }
 
+        public static void main(String[] args) {
+        Complex a1 = new Complex(1, 2);
+        Complex a2 = new Complex(1, 3);
+
+        double num = 5;
+
+        System.out.println(a1 + " + " + a2 + " = " + a1.add(a2));
+        System.out.println(a1 + " - " + a2 + " = " + a1.subtract(a2));
+        System.out.println(a1 + " * " + a2 + " = " + a1.multiply(a2));
+        System.out.println(a1 + " / " + a2 + " = " + a1.divide(a2));
+
+        System.out.println(a1 + " + " + num + " = " + a1.add(num));
+        System.out.println(a1 + " - " + num + " = " + a1.subtract(num));
+        System.out.println(a1 + " * " + num + " = " + a1.multiply(num));
+        System.out.println(a1 + " / " + num + " = " + a1.divide(num));
+    }
+
+
 }
